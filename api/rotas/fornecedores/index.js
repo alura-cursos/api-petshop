9d43feba980ca9cd059676fe1781c86c924108fe
Matheus Hernandes <midia.matheus@gmail.com>
2020-07-14 15:54:32 -0300

    video3.2

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -4,6 +4,7 @@ const Fornecedor = require('./Fornecedor')
 
 roteador.get('/', async (requisicao, resposta) => {
     const resultados = await TabelaFornecedor.listar()
+    resposta.status(200)
     resposta.send(
         JSON.stringify(resultados)
     )
@@ -14,6 +15,7 @@ roteador.post('/', async (requisicao, resposta) => {
         const dadosRecebidos = requisicao.body
         const fornecedor = new Fornecedor(dadosRecebidos)
         await fornecedor.criar()
+        resposta.status(201)
         resposta.send(
             JSON.stringify(fornecedor)
         )
@@ -31,6 +33,7 @@ roteador.get('/:idFornecedor', async (requisicao, resposta) => {
         const id = requisicao.params.idFornecedor
         const fornecedor = new Fornecedor({ id: id })
         await fornecedor.carregar()
+        resposta.status(200)
         resposta.send(
             JSON.stringify(fornecedor)
         )
@@ -50,6 +53,7 @@ roteador.put('/:idFornecedor', async (requisicao, resposta) => {
         const dados = Object.assign({}, dadosRecebidos, { id: id })
         const fornecedor = new Fornecedor(dados)
         await fornecedor.atualizar()
+        resposta.status(204)
         resposta.end()
     } catch (erro) {
         resposta.send(
@@ -66,6 +70,7 @@ roteador.delete('/:idFornecedor', async (requisicao, resposta) => {
         const fornecedor = new Fornecedor({ id: id })
         await fornecedor.carregar()
         await fornecedor.remover()
+        resposta.status(204)
         resposta.end()
     } catch (erro) {
         resposta.send(
